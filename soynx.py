c573dbe90e71f0abf6a98885da36f08d95a11c17
P0P01-dnce <kendrickvella57@gmail.com>
2025-05-23 19:51:41 -0500

    PrimerCommit

---
+++ soynx.py
@@ -0,0 +1,30 @@
+from scapy.all import *
+
+def scanner(ip , port):
+    pket = IP(dst=ip) / TCP(dport = port , flags="S")
+    resp = sr1(pket, timeout=1,verbose=0)
+    if resp and resp.haslayer(TCP) and resp.getlayer(TCP).flags == 0x12:
+        return True
+    return False
+#def proxy_changes()
+def ingreso():
+        ip_capture = input("[Enter ip]>")
+        for p in range(20,1025):
+            if scanner(ip_capture,p):
+                print("----------------------------------------------------\n")
+                print(f"|[+]Puerto {p} abierto para la direccion {ip_capture} |")
+                print("------------------------------------------------------")
+                break
+if __name__ == "__main__":
+    try:
+        while True:
+            print("[1]------------Escaneo de Puertos\n")
+            print("[2]-------------Salir\n")
+            opcion = int(input("Ingresa la opcion:"))
+            if opcion == 1:
+                ingreso()
+            elif opcion == 2:
+                print("Saliendo....")
+                break
+    except ValueError:
+        print("Ingresa un dato correcto......")
